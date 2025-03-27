cfedfec8afffa70cfc7eebda7b652f3b62d3f80c
spartak.gareginyan <gareginyans@mail.ru>
2025-03-27 10:52:57 +0400

    add comments in the schema.graphqls

--- src/main/java/com/xcoder/graphqlbooks/Application.java
+++ src/main/java/com/xcoder/graphqlbooks/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlBooksApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphqlBooksApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
